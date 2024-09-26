b43049889dc011e658bcfe8b52fae8c09e020b8f
Vahe Arzumanyan <varvolta@gmail.com>
2024-09-26 11:07:43 +0400

    app

--- src/api/index.js
+++ src/api/index.js
@@ -32,7 +32,7 @@ class Api {
         body,
         headers = {},
     }) => {
-        url = `${this.cleanReq ? this.URL : 'https://localhost:3000/api'}${this.URL
+        url = `${this.cleanReq ? this.URL : 'https://autohackers.am:3000/api'}${this.URL
             }${url}`
 
         const options = {
